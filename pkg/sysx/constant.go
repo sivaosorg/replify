dd586fab4ebc6423fe163df75ad7cc9493b7d119
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2026-04-07 15:33:30 +0700

    :mute: silent changes: add open-flags constants #2

--- pkg/sysx/constant.go
+++ pkg/sysx/constant.go
@@ -1,1 +1,48 @@
 package sysx
+
+import "os"
+
+// FileOpenFlags is a convenience type for file open flags.
+const (
+	// os.O_CREATE | os.O_WRONLY | os.O_APPEND
+	CWA = os.O_CREATE | os.O_WRONLY | os.O_APPEND
+
+	// os.O_CREATE | os.O_WRONLY | os.O_TRUNC
+	CWT = os.O_CREATE | os.O_WRONLY | os.O_TRUNC
+
+	// os.O_CREATE | os.O_WRONLY
+	CW = os.O_CREATE | os.O_WRONLY
+
+	// os.O_APPEND | os.O_WRONLY
+	AW = os.O_APPEND | os.O_WRONLY
+
+	// os.O_TRUNC | os.O_WRONLY
+	TW = os.O_TRUNC | os.O_WRONLY
+
+	// os.O_RDONLY
+	RO = os.O_RDONLY
+
+	// os.O_RDWR
+	RW = os.O_RDWR
+
+	// os.O_RDWR | os.O_CREATE
+	CRW = os.O_RDWR | os.O_CREATE
+
+	// os.O_RDWR | os.O_CREATE | os.O_TRUNC
+	CRWT = os.O_RDWR | os.O_CREATE | os.O_TRUNC
+
+	// os.O_RDWR | os.O_APPEND
+	ARW = os.O_RDWR | os.O_APPEND
+
+	// os.O_RDWR | os.O_APPEND | os.O_CREATE
+	CARW = os.O_RDWR | os.O_APPEND | os.O_CREATE
+
+	// os.O_RDWR | os.O_APPEND | os.O_TRUNC
+	TARW = os.O_RDWR | os.O_APPEND | os.O_TRUNC
+
+	// os.O_RDWR | os.O_TRUNC
+	TRW = os.O_RDWR | os.O_TRUNC
+
+	// os.O_RDWR | os.O_CREATE | os.O_APPEND
+	CRWA = os.O_RDWR | os.O_CREATE | os.O_APPEND
+)

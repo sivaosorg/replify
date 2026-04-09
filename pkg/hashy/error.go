44bb2a2fc90ceae348398bd4daff7c62c8fd3e6b
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2026-04-09 14:23:52 +0700

    :recycle: refactor: update codebase #2

--- pkg/hashy/error.go
+++ pkg/hashy/error.go
@@ -12,5 +12,5 @@ import "fmt"
 //	err := &ErrNotStringer{Field: "name"}
 //	fmt.Println(err.Error()) // "pkg.hash: field \"name\" has hash:\"string\" tag but does not implement fmt.Stringer"
 func (e *ErrNotStringer) Error() string {
-	return fmt.Sprintf("pkg.hash: field %q has hash:\"string\" tag but does not implement fmt.Stringer", e.Field)
+	return fmt.Sprintf(`pkg.hash: field %q has hash:"string" tag but does not implement fmt.Stringer`, e.Field)
 }
